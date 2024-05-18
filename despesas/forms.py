f95b304632cbc35db2e0b0dcfaa2b0f0ffc45a49
fabio <fabio.matosteixeira@gmail.com>
2024-05-18 17:58:59 -0300

    Criação da logica e estilização do formulário de criação de nova despesa

--- despesas/forms.py
+++ despesas/forms.py
@@ -6,3 +6,17 @@ class DespesaModelForm(forms.ModelForm):
     class Meta:
         model = Despesa
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'value': forms.NumberInput(attrs={'class': 'form-control'}),
+            'bank': forms.Select(attrs={'class': 'form-control'}),
+            'month': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
+        labels = {
+            'name': 'Nome',  # Altera o label de 'name' para 'Nome'
+            'value': 'Valor',  # Altera o label de 'value' para 'Valor'
+            'bank': 'Banco',  # Altera o label de 'bank' para 'Banco'
+            'month': 'Meses',  # Altera o label de 'month' para 'Meses'
+            'description': 'Descrição',  # Altera o label de 'description' para 'Descrição'
+        }
